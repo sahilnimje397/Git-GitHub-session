e912f6d91622541f28699b31108189e93e8720a9
sahil-nimje <sahilnimje06@gmail.com>
2025-08-01 12:48:34 +0530

    Added test1 and test2

---
+++ test1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+// This is a simple C program that prints "Hello, World!" to the console.
+// It serves as a basic example of how to write and run a C program.
