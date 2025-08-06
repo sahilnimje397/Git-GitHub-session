591669c34fe986383fa3cf55afff4cd49c9f559b
sahilnimje397 <sahilnimje06@gmail.com>
2025-08-06 16:46:50 +0530

    test10 test1,c

--- test1.c
+++ test1.c
@@ -4,5 +4,4 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
-// This is a simple C program that prints "Hello, World!" to the console.
-// It serves as a basic example of how to write and run a C program.
+
